9528528ea6447ab310e3fa935241c09ee5e04beb
Eric Moskowitz <moskowitz.e@icloud.com>
2023-09-18 11:43:21 -0400

    working form mechanics and requirements.txt

--- bank.py
+++ bank.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/apply', methods=['POST'])
+def apply():
+    response = "submitted!"
+    return render_template("response_area.html", response=response)
+    # if request.method == 'POST':
+    #     #obtain the relevant form data
+    #     lastname = request.form.get('')
+
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(
+        port=8080,
+        debug=True
+        )
